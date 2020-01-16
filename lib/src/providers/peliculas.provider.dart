b235d1708a8d3945472307d175c78af493c10501
felibassx <felibass@gmail.com>
2020-01-16 11:34:27 -0300

    Pagina Home lista en primera instanci

--- lib/src/providers/peliculas.provider.dart
+++ lib/src/providers/peliculas.provider.dart
@@ -1,28 +1,68 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:peliculas/src/models/peliculas_model.dart';
 import 'package:http/http.dart' as http;
 
 class PeliculasProvider {
-
   String _apiKey = 'f325141b6c6c6ab81333cbef19dec5a7';
   String _url = 'api.themoviedb.org';
   String _languaje = 'es-ES';
+  int _popularesPage = 0;
+  bool _cargando = false;
 
-  Future<List<Pelicula>> getEnCines() async {
-    
-    final url = Uri.https(_url, '3/movie/now_playing', {
-      'api_key': _apiKey,
-      'language': _languaje
-    });
+  // Manejo de Stream
+  List<Pelicula> _populares = new List();
 
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+  void disposeStream() {
+    _popularesStreamController?.close();
+  }
+  // Manejo de Stream
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final resp = await http.get(url);
     final decodedData = json.decode(resp.body);
 
     final peliculas = new Peliculas.fromJsonList(decodedData['results']);
 
     return peliculas.items;
-
   }
 
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _languaje});
+
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+
+    // controlar la carga de peliculas, para no hacer n llamadas al servicio
+    if ( _cargando ) return [];
+
+    _cargando = true;
+    
+    _popularesPage++;
+
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key': _apiKey,
+      'language': _languaje,
+      'page': _popularesPage.toString()
+    });
+
+    // Utilización del Stream
+    final resp = await _procesarRespuesta(url);
+    _populares.addAll(resp);
+    popularesSink(_populares);
+
+    _cargando = false;
+
+    return resp;
+
+  }
 }
---
+++ lib/src/widgets/movie_horizontal.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/peliculas_model.dart';
+
+class MovieHorizontal extends StatelessWidget {
+
+  final List<Pelicula> peliculas;
+  final _pageController = new PageController(
+    initialPage: 1,
+    viewportFraction: 0.3
+  );
+  final Function siguientePagina;
+
+  MovieHorizontal({@required this.peliculas, @required this.siguientePagina});
+
+  @override
+  Widget build(BuildContext context) {
+    // Para saber el Screen Size
+    final _screenSize = MediaQuery.of(context).size;
+    
+    _pageController.addListener(() {
+
+      if(_pageController.position.pixels >= _pageController.position.maxScrollExtent - 200 ) {
+        siguientePagina();
+      }
+
+    });
+
+    return Container(
+      height: _screenSize.height * 0.2,
+      child: PageView.builder(
+        pageSnapping: false,
+        controller: _pageController,
+        // children: _tarjetas(context),
+        itemCount: peliculas.length,
+        itemBuilder: (contex, i) {
+          return _tarjeta(context, peliculas[i]);
+        },
+      ),
+    );
+  }
+
+  Widget _tarjeta(BuildContext context, Pelicula pelicula) {
+    return Container(
+        margin: EdgeInsets.only(right: 15.0),
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                image: NetworkImage(pelicula.getPosterImg()),
+                placeholder: AssetImage('assets/img/no-image.jpg'),
+                fit: BoxFit.cover,
+                height: 110.0,
+              ),
+            ),
+            SizedBox(height: 5.0,),
+            Text(
+              pelicula.title,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.caption,
+            )
+          ],
+        ),
+      );
+  }
+
+  List<Widget> _tarjetas(BuildContext context) {
+    return peliculas.map((pelicula) {
+      return Container(
+        margin: EdgeInsets.only(right: 15.0),
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                image: NetworkImage(pelicula.getPosterImg()),
+                placeholder: AssetImage('assets/img/no-image.jpg'),
+                fit: BoxFit.cover,
+                height: 110.0,
+              ),
+            ),
+            SizedBox(height: 5.0,),
+            Text(
+              pelicula.title,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.caption,
+            )
+          ],
+        ),
+      );
+    }).toList();
+  }
+}
